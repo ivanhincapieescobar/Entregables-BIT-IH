8b2b2f3889297962d891f7c847ddad7004b3347c
ivanhincapieescobar <ivanhincapieescobar@gmail.com>
2026-08-03 19:43:28 -0500

    Notas clase 3 y actividad

---
+++ Clase-3/Actividad-clase.py
@@ -0,0 +1,18 @@
+#Actividad
+#Actividades de programación básica:
+
+#1. Calcular el area de un cuadrado de lado 4.5 cm
+Area = 4.5
+print("El Area del cuadrado es", Area ** 2)
+
+#2. Calcular el costo total de comprar 35 porciones de pizza si una porcion cuesta $6000
+porciones = 35
+valorunit = 6000
+print("El valor total de de la compra es: ", porciones * valorunit )
+
+#3. Calcular el resultado de multiplicar dos numeros ingresados por el usuario
+print("Bienvenido a la mini calculadora")
+numero_1 = input( "Ingresa un numero: ")
+numero_2 = input( "Ingresa otro numero: ")
+suma = int(numero_1) + int(numero_2)
+print("Resultado de tu suma: ", suma)
